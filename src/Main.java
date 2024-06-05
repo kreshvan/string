88474b257d545804eecac4c972818cdbe2d37a9b
днс <kreshvan12317@yandex.ru>
2024-06-05 22:38:59 +0300

    string.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-   System.out.println("d");
+        //заданиее 1
+        String firstName = "ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = "Ф.И.О.";
+        System.out.println(fullName + " " + "сотрудника" + " " + firstName + " " + middleName + " " + lastName);
+        //задание 2
+        String fullName1 = "Ivan Ivanovich Ivanov";
+        String str = fullName1.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —"+" " + str);
+        //задание 3
+        String fullName2 = "Иванов Семён Семёнович";
+        String str1 = fullName2.replace("ё","е");
+        System.out.println("Данные Ф. И. О. сотрудника —"+" "+str1);
+
     }
 }
